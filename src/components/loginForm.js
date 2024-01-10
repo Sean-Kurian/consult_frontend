bcd33a6944bc06a224d87eb13c474f7979dc0350
Sean-Kurian <kuriansean@gmail.com>
2024-01-10 18:26:57 -0500

    styling changes

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext.js';
-import axios from 'axios';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.js";
+import axios from "axios";
+import "./loginForm.css";
 
 function LoginForm() {
   const { authToken, login, logout } = useAuth();
@@ -17,40 +18,53 @@ function LoginForm() {
   const loginClick = async (e) => {
     e.preventDefault();
 
-    await axios.post(process.env.REACT_APP_BACKEND_URL + "/auth/login", { email, password })
-    .then (result => {
-      if (result.data.success) {
-        login(result.headers.get('Authorization'));
-        navigate('/sow');
-      } else {
+    await axios
+      .post(process.env.REACT_APP_BACKEND_URL + "/auth/login", {
+        email,
+        password,
+      })
+      .then((result) => {
+        if (result.data.success) {
+          login(result.headers.get("Authorization"));
+          navigate("/sow");
+        } else {
+          setShowFailed(true);
+          setErrorText(result.data.message);
+        }
+      })
+      .catch((err) => {
         setShowFailed(true);
-        setErrorText(result.data.message);
-      }
-    })
-    .catch (err => {
-      setShowFailed(true);
-      setErrorText("Something went wrong!");
-    })
-  }
-
+        setErrorText("Something went wrong!");
+      });
+  };
 
   return (
-    <Form>
-      { showFailed && (
-        <Form.Text style={{ color: 'red' }}>
-          { errorText }
-        </Form.Text>)}
+    <Form className="login-form">
+      {showFailed && (
+        <Form.Text style={{ color: "red" }}>{errorText}</Form.Text>
+      )}
       <Form.Group className="mb-3" controlId="loginEmail">
-        <Form.Label>Email address</Form.Label><br />
-        <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} /><br />
+        <Form.Label>Email address</Form.Label>
+        <br />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
+          We will never share your email with anyone else.
         </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="loginPassword">
-        <Form.Label>Password</Form.Label><br />
-        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <Form.Label>Password</Form.Label>
+        <br />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
       <Button variant="primary" type="submit" onClick={(e) => loginClick(e)}>
         Submit
--- src/screens/FileScreen.js
+++ src/screens/FileScreen.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import FileList from "../components/fileList.js";
-import "./File.css";
+import "./FileScreen.css";
 
 function FileScreen(props) {
-    return (
-        <div className="div">
-            <div className="div-2">
-                <div className="div-3">
-                    <FileList />
-                </div>
-            </div>
+  return (
+    <div className="div">
+      <div className="div-2">
+        <div className="div-3">
+          <FileList />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default FileScreen;
